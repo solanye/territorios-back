9d4af1d0aa2f4c620faa1caeb883d30fe3b093c9
solanye <solangel.002@hotmail.com>
2026-03-21 08:54:51 -0500

    first configuration

---
+++ src/main/java/com/territorio/territorios/TerritoriosApplication.java
@@ -0,0 +1,13 @@
+package com.territorio.territorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerritoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerritoriosApplication.class, args);
+	}
+
+}
